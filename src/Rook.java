bd5456e18d550f76208bab6301c24fa80b250d7e
bear <maphick@yandex.ru>
2023-09-10 23:38:59 +0300

    Rook done

---
+++ src/Rook.java
@@ -0,0 +1,27 @@
+// write your code here
+public class Rook extends ChessPiece{
+    public Rook(String color) {
+        super(color);
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    public boolean checkPos(int pos) {
+        return pos >= 0 && pos <= 7;
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        boolean horizontalMove  = (toLine == line) && (toColumn!= column);
+        boolean verticalMove = (toColumn == column) && (toLine != line);
+        return (horizontalMove || verticalMove) && checkPos(toLine) && checkPos(toColumn);
+    }
+
+    @Override
+    public String getSymbol() {
+        return "R";
+    }
+}
